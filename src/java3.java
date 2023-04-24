8ea9203b4fce2cce3e552760a1b6fd1549d68459
Nikita Troshin <nikitas.troshin@mail.ru>
2023-04-24 20:48:40 +0300

    Initial commit

---
+++ src/java3.java
@@ -0,0 +1,11 @@
+public class java3 {
+
+    public static void main(String[] args) {
+        int price = 10000;
+
+        int mile = price / 20;
+
+        System.out.println("Бонусных миль начислено:" + mile);
+
+    }
+}
